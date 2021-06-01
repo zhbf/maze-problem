aded231f1e7a05404b891dac605f574e13e1e053
zhoubangfu <zbfcqtl@163.com>
2021-06-01 14:18:29 +0800

    chore: 统一用户名

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
-# maze-problem（[之间](https://zhoubangfu.com)）
+# maze-problem（[之间](https://imbf.cc)）
 
 ## 前言
-突然想起大学时老师让用Java GUI解决迷宫问题，当时还真给做出来了，可惜代码不见了
+
+突然想起大学时老师让用 Java GUI 解决迷宫问题，当时还真给做出来了，可惜代码不见了
 
 ## 预览
-[maze-problem](https://zhoubangfu.com/tool.html#/maze)
+
+[maze-problem](https://imbf.cc/tool.html#/maze)
 
 ## 介绍
-本项目意在使用JavaScript解决迷宫问题（使用vue-cli@3搭建环境），计算所有通路条数，并展示最优解之一
 
-___迷宫最大规格为10*10___，__6*7无障碍通路为20562673，计算量较大，推荐高于此比例时生成障碍，避免死机__
+本项目意在使用 JavaScript 解决迷宫问题（使用 vue-cli@3 搭建环境），计算所有通路条数，并展示最优解之一
+
+**_迷宫最大规格为 10\*10_**，**6\*7 无障碍通路为 20562673，计算量较大，推荐高于此比例时生成障碍，避免死机**
 
 ## 使用方式
+
 ```shell
 # 克隆项目
 git clone https://github.com/zhbf/maze-problem.git
@@ -28,4 +32,4 @@ npm run start
 
 错误优化项请留言
 
-### 待更😁
+### 待更 😁
--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
   <div id="app">
     <div class="main">
       <header class="maze-header">
-        <h2>迷宫问题实现-<a href="https://zhoubangfu.com/">之间</a></h2>
-        <sub> 图标来自
-          <a
-            href="https://www.easyicon.net"
-            target="_blank">easyicon</a>
+        <h2>迷宫问题实现-<a href="https://imbf.cc/">之间</a></h2>
+        <sub>
+          图标来自
+          <a href="https://www.easyicon.net" target="_blank">easyicon</a>
         </sub>
       </header>
       <div class="maze-attr">
@@ -41,227 +40,228 @@
         </el-form>
       </div>
       <div class="maze-body">
-        <div
-          class="maze-row"
-          v-for="i of form.row"
-          :key="`row${i}`">
+        <div class="maze-row" v-for="i of form.row" :key="`row${i}`">
           <div
             class="maze-step"
             v-for="j of form.col"
-            :class="mazeStyle[displayMaze[i-1][j-1]]"
+            :class="mazeStyle[displayMaze[i - 1][j - 1]]"
             :key="`col${j}`"
-            @click="changeStatus(i,j)"
+            @click="changeStatus(i, j)"
           ></div>
         </div>
       </div>
-      <footer class="maze-result">当前迷宫有{{result.routeCount}}条出路，图中显示为最优解之一</footer>
+      <footer class="maze-result">
+        当前迷宫有{{ result.routeCount }}条出路，图中显示为最优解之一
+      </footer>
     </div>
   </div>
 </template>
 
 <script>
-  import {initMaze, redisplay, findAllRoutes} from '@/utils'
+import { initMaze, redisplay, findAllRoutes } from '@/utils';
 
-  /**
-   * 规则0-可过，1-障碍，2-死胡同，3-往右，4-往下，5-往左，6-往上
-   */
-  export default {
-    data() {
-      return {
-        form: {
-          row: 4,
-          col: 4
-        },
-        // 表单限制
-        limit: {
-          min: 2,
-          max: 10
-        },
-        // 迷宫
-        maze: [],
-        displayMaze: [],
-        // 迷宫位置样式
-        mazeStyle: [
-          '',
-          'obstacle',
-          '',
-          'go-right',
-          'go-down',
-          'go-left',
-          'go-up'
-        ],
-        // 是否已经计算过了
-        // 计算过在布置障碍的时候，需要深度遍历
-        calculated: false,
-        // 计算出的路线总汇
-        result: {routeCount: 0, bestRoute: []}
+/**
+ * 规则0-可过，1-障碍，2-死胡同，3-往右，4-往下，5-往左，6-往上
+ */
+export default {
+  data() {
+    return {
+      form: {
+        row: 4,
+        col: 4,
+      },
+      // 表单限制
+      limit: {
+        min: 2,
+        max: 10,
+      },
+      // 迷宫
+      maze: [],
+      displayMaze: [],
+      // 迷宫位置样式
+      mazeStyle: [
+        '',
+        'obstacle',
+        '',
+        'go-right',
+        'go-down',
+        'go-left',
+        'go-up',
+      ],
+      // 是否已经计算过了
+      // 计算过在布置障碍的时候，需要深度遍历
+      calculated: false,
+      // 计算出的路线总汇
+      result: { routeCount: 0, bestRoute: [] },
+    };
+  },
+  watch: {
+    maze(val) {
+      this.displayMaze = val;
+    },
+  },
+  created() {
+    this.generateMap();
+    this.displayMaze = this.maze;
+  },
+  methods: {
+    // 输入框失去焦点
+    inputBlur() {
+      // 删除后该组件不会赋默认值，手动赋值
+      if (!this.form.row) {
+        this.form.row = 5;
+      } else if (!this.form.col) {
+        this.form.col = 5;
       }
     },
-    watch: {
-      maze(val) {
-        this.displayMaze = val
+    // 生成地图
+    generateMap() {
+      this.maze = initMaze(this.form);
+    },
+    // 生成障碍
+    autoObstacle() {
+      this.maze = initMaze(this.form, true);
+    },
+    // 计算
+    calculate() {
+      // 设置已计算过
+      this.calculated = true;
+      this.result = findAllRoutes(this.maze);
+
+      if (this.result.routeCount === 0) {
+        this.$message.error('没有出路！！！');
+      } else {
+        this.displayMaze = redisplay(this.maze, this.result.bestRoute).maze;
       }
     },
-    created() {
-      this.generateMap()
-      this.displayMaze = this.maze
-    },
-    methods: {
-      // 输入框失去焦点
-      inputBlur() {
-        // 删除后该组件不会赋默认值，手动赋值
-        if (!this.form.row) {
-          this.form.row = 5
-        } else if (!this.form.col) {
-          this.form.col = 5
-        }
-      },
-      // 生成地图
-      generateMap() {
-        this.maze = initMaze(this.form)
-      },
-      // 生成障碍
-      autoObstacle() {
-        this.maze = initMaze(this.form, true)
-      },
-      // 计算
-      calculate() {
-        // 设置已计算过
-        this.calculated = true
-        this.result = findAllRoutes(this.maze)
+    // 在当前位置移除或添加障碍
+    changeStatus(i, j) {
+      // 如果是第一个或最后一个，则退出
+      if ((i === 1 && i === j) || (i === this.form.row && j === this.form.col))
+        return;
 
-        if (this.result.routeCount === 0) {
-          this.$message.error('没有出路！！！')
-        } else {
-          this.displayMaze = redisplay(this.maze, this.result.bestRoute).maze
-        }
-      },
-      // 在当前位置移除或添加障碍
-      changeStatus(i, j) {
-        // 如果是第一个或最后一个，则退出
-        if ((i === 1 && i === j) || (i === this.form.row && j === this.form.col))
-          return
+      // 设置障碍
+      this.maze[i - 1][j - 1] = this.maze[i - 1][j - 1] ? 0 : 1;
 
-        // 设置障碍
-        this.maze[i - 1][j - 1] = this.maze[i - 1][j - 1] ? 0 : 1
-
-        // 重置一下地图
-        if (this.calculated) {
-          this.calculated = false
-          this.maze = this.maze.map(row => row.map(col => col !== 1 ? 0 : 1))
-        } else {
-          // 简单重塑maze
-          this.maze = this.maze.filter(() => true)
-        }
+      // 重置一下地图
+      if (this.calculated) {
+        this.calculated = false;
+        this.maze = this.maze.map((row) =>
+          row.map((col) => (col !== 1 ? 0 : 1))
+        );
+      } else {
+        // 简单重塑maze
+        this.maze = this.maze.filter(() => true);
       }
-    }
-  }
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  // 小方格border
-  $step-border: 1px solid;
+// 小方格border
+$step-border: 1px solid;
 
-  #app {
+#app {
+  display: flex;
+  justify-content: center;
+}
+
+// 障碍
+.obstacle {
+  background-image: url('./assets/images/obstacle.png');
+}
+
+// 向左走
+.go-left {
+  background-image: url('./assets/images/left.png');
+}
+
+.go-right {
+  background-image: url('./assets/images/right.png');
+}
+
+.go-up {
+  background-image: url('./assets/images/up.png');
+}
+
+.go-down {
+  background-image: url('./assets/images/down.png');
+}
+
+.main {
+  width: 610px;
+
+  .maze-header {
+    padding: 18px 0;
+
+    h2 {
+      display: inline-block;
+    }
+  }
+
+  .maze-body {
+    width: 100%;
+    height: 600px;
+    padding: 15px;
+    margin-bottom: 15px;
+    box-sizing: border-box;
+    border: 1px solid #cfcfcf;
     display: flex;
-    justify-content: center;
-  }
+    // 让每一行按纵向排列
+    flex-direction: column;
 
-  // 障碍
-  .obstacle {
-    background-image: url("./assets/images/obstacle.png");
-  }
-
-  // 向左走
-  .go-left {
-    background-image: url("./assets/images/left.png");
-  }
-
-  .go-right {
-    background-image: url("./assets/images/right.png");
-  }
-
-  .go-up {
-    background-image: url("./assets/images/up.png");
-  }
-
-  .go-down {
-    background-image: url("./assets/images/down.png");
-  }
-
-  .main {
-    width: 610px;
-
-    .maze-header {
-      padding: 18px 0;
-
-      h2 {
-        display: inline-block;
-      }
-    }
-
-    .maze-body {
-      width: 100%;
-      height: 600px;
-      padding: 15px;
-      margin-bottom: 15px;
-      box-sizing: border-box;
-      border: 1px solid #cfcfcf;
+    .maze-row {
+      // 纵向平均分布
+      flex: 1;
+      // 将自己设置成弹性容器
       display: flex;
-      // 让每一行按纵向排列
-      flex-direction: column;
 
-      .maze-row {
-        // 纵向平均分布
+      .maze-step {
+        cursor: pointer;
+        // 水平平均分布
         flex: 1;
-        // 将自己设置成弹性容器
-        display: flex;
-
-        .maze-step {
-          cursor: pointer;
-          // 水平平均分布
-          flex: 1;
-          // 用div的病
-          display: inline-block;
-          box-sizing: border-box;
-          background: {
-            size: 60%;
-            repeat: no-repeat;
-            position: center center;
-          }
-
-          // 所以小格子都设置左上边框
-          border: {
-            top: $step-border;
-            left: $step-border;
-          }
-
-          // 每行最后一个设置右边框
-          &:last-of-type {
-            border: {
-              right: $step-border;
-            }
-          }
+        // 用div的病
+        display: inline-block;
+        box-sizing: border-box;
+        background: {
+          size: 60%;
+          repeat: no-repeat;
+          position: center center;
         }
 
-        // 第一排第一个
-        &:first-of-type {
-          .maze-step:first-of-type {
-            background-image: url("./assets/images/start.png");
-          }
+        // 所以小格子都设置左上边框
+        border: {
+          top: $step-border;
+          left: $step-border;
         }
 
-        // 最后一列每个格子都设置下边框
+        // 每行最后一个设置右边框
         &:last-of-type {
-          .maze-step {
-            border-bottom: $step-border;
+          border: {
+            right: $step-border;
+          }
+        }
+      }
 
-            &:last-of-type {
-              background-image: url("./assets/images/end.png");
-            }
+      // 第一排第一个
+      &:first-of-type {
+        .maze-step:first-of-type {
+          background-image: url('./assets/images/start.png');
+        }
+      }
+
+      // 最后一列每个格子都设置下边框
+      &:last-of-type {
+        .maze-step {
+          border-bottom: $step-border;
+
+          &:last-of-type {
+            background-image: url('./assets/images/end.png');
           }
         }
       }
     }
   }
+}
 </style>
